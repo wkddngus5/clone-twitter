0e400318a0d57aec674c1f61a721b9e0c447d422
woohyun <brain@ddingdong.net>
2020-10-10 23:45:26 +0900

    feat: login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -6,6 +6,7 @@ const Auth = () => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [newAccount, setNewAccount] = useState(true);
+	const [error, setError] = useState('');
 
 	const onChange = (event) => {
 		const {target: {name, value}} = event;
@@ -29,10 +30,11 @@ const Auth = () => {
 				)
 			}
 		} catch (error) {
-			console.log(error);
+			setError(error.message);
 		}
-
 	}
+
+	const toggleAccount = () => setNewAccount((prev) => !prev);
 
 	return (
 		<div>
@@ -54,7 +56,11 @@ const Auth = () => {
 					onChange={onChange}
 				/>
 				<input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
+				{error}
 			</form>
+			<span onClick={toggleAccount}>
+				{newAccount ? 'Log in' : 'Create Account'}
+			</span>
 			<div>
 				<button>Continue with Google</button>
 				<button>Continue with Github</button>
