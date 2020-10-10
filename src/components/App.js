292fcf1d8dadc14f422feae5882e7f89fb1f8e0a
wkddngus5 <wkddngus5@naver.com>
2020-10-10 23:45:26 +0900

    feat: login

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,24 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import AppRouter from 'components/Router';
 import {authService} from 'fbase';
 import { auth } from 'firebase';
 
 function App() {
+	const [init, setInit] = useState(false);
 	const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+	useEffect(() => {
+		authService.onAuthStateChanged((user) => {
+			if (user) {
+				setIsLoggedIn(true);
+			} else {
+				setIsLoggedIn(false);
+			}
+			setInit(true);
+		});
+	}, []);
 	return (
 		<>
-			<AppRouter
-				isLoggedIn={isLoggedIn} />
+			{init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'  }
 			<footer>&copy; Twitter {new Date().getFullYear()}</footer>
 		</>
 	);
