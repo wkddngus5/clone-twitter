039616fc93a3235054e7eae7c15abd5917c6b9c5
woohyun <brain@ddingdong.net>
2020-10-10 23:52:34 +0900

    feat: social login

--- src/fbase.js
+++ src/fbase.js
@@ -13,4 +13,5 @@ const firebaseConfig = {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  export const firebaseInstance = firebase;
   export const authService = firebase.auth();
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { authService } from 'fbase';
+import { authService, firebaseInstance } from 'fbase';
 
 
 const Auth = () => {
@@ -35,7 +35,17 @@ const Auth = () => {
 	}
 
 	const toggleAccount = () => setNewAccount((prev) => !prev);
-
+	const onSocialClick = async (event) => {
+		const {target: {name}} = event;
+		let provider;
+		if (name === 'google') {
+			provider = new firebaseInstance.auth.GoogleAuthProvider();
+		} else if (name === 'github') {
+			provider = new firebaseInstance.auth.GithubAuthProvider();
+		}
+		const data = await authService.signInWithPopup(provider);
+		console.log(data);
+	} 
 	return (
 		<div>
 			<form onSubmit={onSubmit}>
@@ -62,8 +72,8 @@ const Auth = () => {
 				{newAccount ? 'Log in' : 'Create Account'}
 			</span>
 			<div>
-				<button>Continue with Google</button>
-				<button>Continue with Github</button>
+				<button name="google" onClick={onSocialClick}>Continue with Google</button>
+				<button name="github" onClick={onSocialClick}>Continue with Github</button>
 			</div>
 		</div>
 	)
