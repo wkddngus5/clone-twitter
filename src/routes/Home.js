9a8dd5fa5b9dfb50e388dd001b348cf5f2e5c3f5
wkddngus5 <wkddngus5@naver.com>
2020-10-11 14:16:03 +0900

    feat: form and database setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+	const [tweet, setTweet] = useState('');
+	const onSubmit = (event) => {
+		event.preventDefault();
+	}
+	const onChange = (event) => {
+		const {target:{value}} = event;
+		setTweet(value);
+	}
+	return (
+		<div>
+			<form onSubmit={onSubmit}>
+				<input
+					type="text"
+					placeholder="What's on your mind?"
+					maxLength={120}
+					onChange={onChange} />
+				<input type="submit" value="tweet" />
+			</form>
+		</div>
+	)
+}
 export default Home;
