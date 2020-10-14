33db477a97266ee280c2e783192e8725711ce541
wkddngus5 <wkddngus5@naver.com>
2020-10-14 17:11:53 +0900

    Updates

---
+++ precache-manifest.63a8bb0c76daafaa327e384f765ed811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52108e570af01d58a32c93dacc2b2034",
+    "url": "/clone-twitter/index.html"
+  },
+  {
+    "revision": "c1a354d96a5d5a5c9bc2",
+    "url": "/clone-twitter/static/css/main.d2ea9130.chunk.css"
+  },
+  {
+    "revision": "5bdaa73f423d31328e7a",
+    "url": "/clone-twitter/static/js/2.30818f7f.chunk.js"
+  },
+  {
+    "revision": "c632640d265671a6e6eee1edde0b38ef",
+    "url": "/clone-twitter/static/js/2.30818f7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1a354d96a5d5a5c9bc2",
+    "url": "/clone-twitter/static/js/main.9e29115c.chunk.js"
+  },
+  {
+    "revision": "e0220921246d1e52ecf2",
+    "url": "/clone-twitter/static/js/runtime-main.8b1ab00f.js"
+  }
+]);
